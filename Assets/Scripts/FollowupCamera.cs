7500b67abe18f778bea063f45933f445b29c1e97
MSI\Aniruddha Tarodekar <tarodekaraniruddha@gmail.com>
2020-09-30 23:59:37 -0400

    raptor blend tree

---
+++ Assets/Scripts/FollowupCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowupCamera : MonoBehaviour
+{
+    
+    [SerializeField] Transform player;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position;
+    }
+}
